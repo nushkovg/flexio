decf18fce29e65b0ff76b0591c5143305da4801a
Goran Nushkov <gnuskov@protonmail.com>
2018-08-20 23:17:24 +0200

    nothing changed, buggy editor fix

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 DEBUG = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-SERVER_NAME = "local.host:8000"
+SERVER_NAME = "localhost:8000"
 SECRET_KEY = 'insecurekeyfordev'
 
 # Flask-Mail.
